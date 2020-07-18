7b7411b59b907b729b94693387064c8453817a29
Ekkasit Pinyoanuntapong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2020-07-18 16:28:57 -0400

    add build

---
+++ precache-manifest.cce61561b84c581d8d7a2f5933bbc8cc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85ec7ea8a5f6520d90277d69478b3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7afc5dc44d1ac58434",
+    "url": "/static/css/main.7c338ba2.chunk.css"
+  },
+  {
+    "revision": "c5acd3057adc3088801a",
+    "url": "/static/js/2.c944f061.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.c944f061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7afc5dc44d1ac58434",
+    "url": "/static/js/main.907e6885.chunk.js"
+  },
+  {
+    "revision": "8ee25d299efa9ce0ac38",
+    "url": "/static/js/runtime-main.321a3b44.js"
+  },
+  {
+    "revision": "bf05915e7f6003b833210d8cf2100727",
+    "url": "/static/media/background.bf05915e.jpg"
+  },
+  {
+    "revision": "f1a4f562e192d8dd52492e0a61f91964",
+    "url": "/static/media/background.f1a4f562.jpg"
+  },
+  {
+    "revision": "0dd2a97ea36a7b72eb066d062caa8a19",
+    "url": "/static/media/line.0dd2a97e.png"
+  },
+  {
+    "revision": "f41d91c6c1c307fb3b1594f49a58e785",
+    "url": "/static/media/profile.f41d91c6.png"
+  },
+  {
+    "revision": "eebe66f52cd9e8c1fb48ffa99c88b774",
+    "url": "/static/media/wechat.eebe66f5.png"
+  }
+]);
