5f7ba0fdcd6a8373882ca9d5f2bee5e3f8567c33
Ekkasit Pinyoanuntapong <Ekkasit.Pinyoanunatpon@coxautoinc.com>
2020-07-18 16:22:25 -0400

    Publishing to gh-pages

--- code/src/redux/store/configureStore.prod.js
+++ code/src/redux/store/configureStore.prod.js
@@ -1,7 +1,7 @@
 import { createStore, compose, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
-import indexReducer from "../redux/indexReducer";
-import indexSaga from "../redux/indexSaga";
+import indexReducer from "../indexReducer";
+import indexSaga from "../indexSaga";
 
 export default function configureStore(initialState) {
   const sagaMiddleWare = createSagaMiddleware();
